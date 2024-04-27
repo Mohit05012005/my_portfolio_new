653e7d1645fde0b92bad0ab5f14744247f2a35a2
Mohit05012005 <147084436+Mohit05012005@users.noreply.github.com>
2024-04-27 23:49:29 +0530

    Update hobbi.js

--- hobbi.js
+++ hobbi.js
@@ -21,34 +21,6 @@ images.forEach(function(image) {
     });
 });
 
-// document.addEventListener('keydown',(en)=>{
-//     if(comment.value){
-//         if(en.key === 'Enter'){
-//             // console.log("enter key is pressed");
-//             // console.log(comment.value);
-//               let div = document.createElement('div');
-//               div.setAttribute('class','myclass');
-//               div.innerHTML = `<div style="background-color:blue;; class= "mycomment">
-//               <p  id="comim" style= "font-size:4rem; margin:30px;"> ${comment.value}</p>
-//               </div>
-//               `
-//               document.body.appendChild(div);
-//             //   let box = document.querySelector('.mycomment');
-//             //   box.style.width = "300px";
-//             //   box.style.height = "200px";
-//             //   box.style.backgroundColor = "blue";
-//             //   innerdiv.style.width = "300px";
-//             //   innerdiv.style.height = "200px";
-             
-             
-//             //   let myclass = document.querySelector('.myclass');
-//             //   myclass.style.color = "red";
-//             //  myclass.style.marginLeft = "200px";
-          
-//         }
-//     }
-    
-// })
 
 
 
